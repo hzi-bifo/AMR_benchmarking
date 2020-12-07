9079350e45ae7674139865091224d1f757f98ab8
Kaixin <hukaixin1@outlook.com>
2020-12-07 16:39:03 +0100

    Update taxid.py

--- taxid.py
+++ taxid.py
@@ -1,6 +1,6 @@
 from ete3 import NCBITaxa
 ncbi = NCBITaxa()
-
+#for each file name in the data storage directory
 with open('i.txt') as input_file:
     for line in input_file:
         print line
@@ -8,7 +8,7 @@ with open('i.txt') as input_file:
             id=line[line.find('taxid__')+7:line.find('__NC')]
         else:
             id = line[line.find('taxid__') + 7:line.find('__AC')]
-
+        #lineage w.r.t. each taxa id
         lineage = ncbi.get_lineage(id)
         #rank=ncbi.get_rank(lineage)
         print lineage
