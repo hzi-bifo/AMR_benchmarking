6ac7a8fa430e60dcba8820f41368c323dcf1d273
augustkx <hukaixin1@outlook.com>
2021-04-17 21:56:38 +0200

    Res

--- ResFinder/Kaixin_Predictions_Res_PointFinder_tools.py
+++ ResFinder/Kaixin_Predictions_Res_PointFinder_tools.py
@@ -9,7 +9,8 @@ from subprocess import PIPE, run
 import pandas as pd
 import numpy as np
 import ast
-
+from os import walk
+from itertools import repeat
 from collections import defaultdict
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import confusion_matrix
@@ -17,16 +18,18 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import f1_score
 import argparse
 
-def model(species,antibiotics,level,tool):
-    path_to_pointfinder="Results/Point_results"+str(species.replace(" ", "_"))+"/"
-    path_to_resfinder="Results/Res_results_"+str(species.replace(" ", "_"))+"/"
+def determination(species,antibiotics,level,tool):
+    path_to_pointfinder="/net/flashtest/scratch/khu/benchmarking/Results/Point_results_"+str(species.replace(" ", "_"))
+    path_to_resfinder="/net/flashtest/scratch/khu/benchmarking/Results/Res_results_"+str(species.replace(" ", "_"))
+    path_to_pr="/net/flashtest/scratch/khu/benchmarking/Results/" + str(species.replace(" ", "_"))
     print(species)
     antibiotics_selected = ast.literal_eval(antibiotics)
 
     print('====> Select_antibiotic:', len(antibiotics_selected), antibiotics_selected)
 
-    # for anti in antibiotics_selected:
-    for anti in ['aztreonam']:
+    for anti in antibiotics_selected[0:2]:
+        print(anti,'--------------------------------------------------------------------')
+    # for anti in ['aztreonam']:
         # save_name_meta, save_name_modelID = amr_utility.name_utility.save_name_modelID(level, species, anti)
         save_name_modelID = 'metadata/model/' + str(level) + '/Data_' + str(species.replace(" ", "_")) + '_' + str(
             anti.translate(str.maketrans({'/': '_', ' ': '_'})))
@@ -38,72 +41,129 @@ def model(species,antibiotics,level,tool):
         y_pre =list()
         samples=data_sub_anti['genome_id'].to_list()
 
-        for strain_ID in samples:
-            # print("strain_ID",strain_ID)
-            # print('-----------------')
-            point = open("%s/%s/pheno_table.txt" % (path_to_pointfinder, strain_ID), "r")
-            res=open("%s/%s/pheno_table.txt" % (path_to_resfinder, strain_ID), "r")
-            lines_start_read = 16
-            if tool=="point":
-                file=point
-            else:
-                file=res
+        for strain_ID in samples[0:1]:
 
+            # lines_start_read = 16
+            if tool=="point":
+                point = open("%s/%s/pheno_table.txt" % (path_to_pointfinder, strain_ID), "r")
+                file=point
+            elif tool=="res":
+                res = open("%s/%s/pheno_table.txt" % (path_to_resfinder, strain_ID), "r")
+                file=res
+            else:
+                both = open("%s/%s/pheno_table.txt" % (path_to_pr, strain_ID), "r")
+                file = both
+                #only applied for debug, check and run ResFinder
+                #check the file exsistence
+                # _, _, f_check = next(walk("%s/%s/pheno_table.txt" % (path_to_pr, strain_ID)))
+                # if "pheno_table.txt" in f_check:
+                #
+                #     both = open("%s/%s/pheno_table.txt" % (path_to_pr, strain_ID), "r")
+                #     file = both
+                # else:
+                #     path_data = "/net/projects/BIFO/patric_genome/"
+                #     run_Res(path_data, strain_ID, species)
+                #     both = open("%s/%s/pheno_table.txt" % (path_to_pr, strain_ID), "r")
+                #     file = both
+            # Read prediction info---------------------------------------------------------------------------
+            pheno_table=pd.DataFrame(columns=['Antimicrobial', 'Class', 'WGS-predicted phenotype', 'Match','Genetic background'])
             for position, line in enumerate(file):
 
-                if position >= lines_start_read:
-                    try:
-                        if anti == line.split()[0]:
-                            # print(line)
-                            # print(line.split()[2])
-                            if line.split()[2] == "No":
 
-                                y_pre.append(0)
+                if "# Antimicrobial	Class" in line:
+                    start=position
 
-                            else:
-                                y_pre.append(1)
-                    except:
-                        pass
+                if "# WARNING:" in line:
+                    end=position
+                    print(end, '!!!!!!!')
+            pheno_table_i = 0
+
+            for position, line in enumerate(file):
+                    #starting to record into dataframe
+                    print('check?')
+                    print(position)
+                    if position>start & position<end :
+                        pheno_table.loc[pheno_table_i] = line
+                        pheno_table_i+=1
+                        print(line)
+            print(pheno_table)
+                # if position >= lines_start_read:
+                #     try:
+                #         if anti == line.split()[0]:
+                #             # print(line)
+                #             # print(line.split()[2])
+                #             if line.split()[2] == "No":
+                #
+                #                 y_pre.append(0)
+                #
+                #             else:
+                #                 y_pre.append(1)
+                #     except:
+                #         pass
 
             # to_exclude = np.arange(0,16)
             # line= pd.read_csv(point,sep="\t", skiprows = to_exclude)
             # print(line.columns)
 
-
-        print(y_pre)
-
-
-
-        y = data_sub_anti['resistant_phenotype'].to_numpy()
-
-
-        print(len(y))
-        print("mcc results")
-        print(matthews_corrcoef(y, y_pre))
-
-
+        if len(y_pre)>0:
+            print('y_pre',len(y_pre))
+            y = data_sub_anti['resistant_phenotype'].to_numpy()
+            print('y',len(y))
+            print("mcc results")
+            print(matthews_corrcoef(y, y_pre))
 
         ###confussion matrix
-        tn, fp, fn, tp = confusion_matrix(y, y_pre).ravel()
+            tn, fp, fn, tp = confusion_matrix(y, y_pre).ravel()
 
-        print("sensitivity", float(tp) / (tp + fn))
-        print("specificity", float(tn) / (tn + fp))
+            print("sensitivity", float(tp) / (tp + fn))
+            print("specificity", float(tn) / (tn + fp))
+            report=classification_report(y, y_pre, labels=[0, 1], output_dict=True)
+            print(report)
 
-        print(classification_report(y, y_pre, labels=[0, 1]))
+            correct_results2 = []
+            for each in y:
+                correct_results2.append(int(each))
 
-        correct_results2 = []
-        for each in y:
-            correct_results2.append(int(each))
+            prediction_results2 = []
+            for each in y_pre:
+                prediction_results2.append(int(each))
 
-        prediction_results2 = []
-        for each in y_pre:
-            prediction_results2.append(int(each))
+            print("f1_score", f1_score(correct_results2, prediction_results2))
+            df = pd.DataFrame(report).transpose()
+            df.to_csv('Results/summary/'+str(species.replace(" ", "_")) + '_' + str(anti.translate(str.maketrans({'/': '_', ' ': '_'})))+'.txt', sep="\t")
+        else:
+            print("No information for antibiotic: ", anti)
 
-        print("f1_score", f1_score(correct_results2, prediction_results2))
+def make_visualization(species,antibiotics,level,tool,score):
+    '''
+    make final summary
+    :return:
+    '''
+    # path_to_pointfinder = "Results/Point_results" + str(species.replace(" ", "_")) + "/"
+    # path_to_resfinder = "Results/Res_results_" + str(species.replace(" ", "_")) + "/"
+    # path_to_pr = "Results/" + str(species.replace(" ", "_")) + "/"
+    print(species)
+    antibiotics_selected = ast.literal_eval(antibiotics)
 
+    print('====> Select_antibiotic:', len(antibiotics_selected), antibiotics_selected)
+    final=pd.DataFrame(index=antibiotics_selected, columns=['f1-score','precision', 'recall','accuracy'] )
+    print(final)
+    for anti in antibiotics_selected:
+        print(anti, '--------------------------------------------------------------------')
+        try:
+            data = pd.read_csv('Results/summary/'+str(species.replace(" ", "_")) + '_' + str(anti.translate(str.maketrans({'/': '_', ' ': '_'})))+'.txt', sep="\t")
+            print(data)
+            final.loc[str(anti),'f1-score']=data.iloc[3,3]
+            final.loc[str(anti),'precision'] = data.iloc[3, 1]
+            final.loc[str(anti),'recall'] = data.iloc[3, 2]
+            final.loc[str(anti),'accuracy'] = data.iloc[2, 2]
+            print(final)
+        except:
+            pass
+    final=final.astype(float).round(2)
+    final.to_csv('Results/summary/' + str(species.replace(" ", "_")) + '.csv', sep="\t")
 
-
-def extract_info(s,l,tool):
+def extract_info(s,l,tool,visualize,score):
 
     data = pd.read_csv('metadata/'+str(l)+'_Species_antibiotic_FineQuality.csv', index_col=0, dtype={'genome_id': object}, sep="\t")
     data = data[data['number'] != 0]# drop the species with 0 in column 'number'.
@@ -115,25 +175,33 @@ def extract_info(s,l,tool):
     df_species = data.index.tolist()
     antibiotics = data['modelling antibiotics'].tolist()
     print(data)
+    if visualize == False:
+        for df_species,antibiotics in zip(df_species, antibiotics):
+            determination(df_species,antibiotics,l,tool)
+    else:
+        for df_species,antibiotics in zip(df_species, antibiotics):
+            make_visualization(df_species,antibiotics,l,tool,score)
 
-    for df_species,antibiotics in zip(df_species, antibiotics):
-        model(df_species,antibiotics,l,tool)
+
+
 
 if __name__== '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--l','--level',default=None, type=str, required=True,
                         help='Quality control: strict or loose')
-    # parser.add_argument('-b', '--balance', dest='balance',
-    #                     help='use downsampling or not ', action='store_true', )
-    parser.add_argument('--t', '--tool', default=None, type=str, required=True,
-                        help='Res, Point')
+
+    parser.add_argument('--t', '--tool', default='Both', type=str, required=True,
+                        help='res, point, both')
     parser.add_argument('--s','--species', default=[], type=str,nargs='+',help='species to run: e.g.\'seudomonas aeruginosa\' \
      \'Klebsiella pneumoniae\' \'Escherichia coli\' \'Staphylococcus aureus\' \'Mycobacterium tuberculosis\' \'Salmonella enterica\' \
      \'Streptococcus pneumoniae\' \'Neisseria gonorrhoeae\'')
-
+    parser.add_argument('-v', '--visualize', dest='v',
+                        help='visualize the final outcome ', action='store_true', )
+    parser.add_argument('--score', default='f1-score', type=str, required=False,
+                        help='Score:f1-score, precision, recall, all. All scores are macro.')
     #parser.set_defaults(canonical=True)
     parsedArgs=parser.parse_args()
     # parser.print_help()
     print(parsedArgs)
-    extract_info(parsedArgs.s, parsedArgs.l,parsedArgs.t)
+    extract_info(parsedArgs.s, parsedArgs.l,parsedArgs.t,parsedArgs.v,parsedArgs.score)
     # extract_info(parsedArgs.s,parsedArgs.b,parsedArgs.l)
