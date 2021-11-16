dd04c35fc0e818afb4a1a86f52aaab8986204ab4
augustkx <hukaixin1@outlook.com>
2021-11-16 23:10:55 +0100

    codes rewrite from scratch

---
+++ PhenotypeSeeker/kmer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+path="/vol/projects/BIFO/patric_genome/"
+mkdir K-mer_lists
+cat ./metadata/genome_list|
+while read i; do
+    glistmaker "${path}${i}.fna"  -o K-mer_lists/${i}_0 -w 13 -c 1
+    glistquery K-mer_lists/${i}_0_13.list  >  K-mer_lists/${i}_mapped
+
+done
--- PhenotypeSeeker/kmer_bench.py
+++ PhenotypeSeeker/kmer_bench.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+# @Author: kxh
+# @Date:   Nov 8 2021
+# @Last Modified by:
+# @Last Modified time:
 import sys
 import os
 sys.path.append('../')
@@ -12,6 +16,8 @@ from itertools import repeat
 import multiprocessing as mp
 import amr_utility.name_utility
 import amr_utility.math_utility
+import amr_utility.file_utility
+import amr_utility.load_data
 # from amr_utility import name_utility
 import warnings
 import argparse
@@ -20,87 +26,103 @@ import logging
 import math
 
 
-def get_feature_vector(cls):
-    # Feature vector loop
-    iterate_to_union = [[x] for x in list(Input.samples.values())]
-    for i in range(math.log(cls.no_samples, 2).__trunc__()):
-        iterate_to_union = [x[0] for x in iterate_to_union]
-        iterate_to_union = [
-            iterate_to_union[j: j + 4 if len(iterate_to_union) < j + 4 else j + 2] for j in
-            range(0, len(iterate_to_union), 2) if j + 2 <= len(iterate_to_union)
-        ]
-        Input.pool.map(partial(cls.get_union, round=i), iterate_to_union)
-    call(["mv %s K-mer_lists/feature_vector.list" % cls.union_output[-1]], shell=True)
-    # [(lambda x: call(["rm -f {}".format(x)], shell=True))(union) for union in cls.union_output[:-1]]
-
-def kmer():
-    #1.Generating the k-mer lists for input samples
-    #bash glistmaker
-
-    #2.Generating the k-mer feature vector.
-
-
-    #3.Mapping samples to the feature vector space
-    # glistquery, split
 
 
 
+def get_kmer(level,log_addr,k, species,antibiotics, n_jobs):
+    # 1.Generating the k-mer lists for every samples for each species
+    #using scripts named kmer.sh
+    # glistmaker
+    # glistquery
+
+    # 2. Mapping samples to the feature vector space
+    antibiotics_selected = ast.literal_eval(antibiotics)
+    print(species)
+    print('====> Select_antibiotic:', len(antibiotics_selected), antibiotics_selected)
+    antibiotics, ID, Y = amr_utility.load_data.extract_info(species, False, level)
+    #
+    # antibiotics, ID, Y = antibiotics[5:], ID[5:], Y[5:]
+    i_anti = 0
+    for anti in antibiotics:
+        vocab = amr_utility.math_utility.vocab_build(False, k)  # all the kmer combinations in list
+        data = np.zeros((len(vocab), 1), dtype='uint16')
+        feature_matrix = pd.DataFrame(data, index=vocab, columns=['initializer'])  # delete later
+        feature_matrix.index.name = 'feature'
+
+        _, meta_temp,_=amr_utility.name_utility.Pts_GETname(level, species, anti,'')
+        # l = 0
+        for i in ID[i_anti]: #todo check
+            print(i)
+            # l += 1
+            # if (l % 1000 == 0):
+            #     print(l, species)
+            # map feature txt from stored data to feature matrix.
+            f = pd.read_csv('K-mer_lists/'+str(i)+'_mapped',
+                            names=['combination', str(i)], dtype={'genome_id': object}, sep="\t")
+            # print('f.shape')
+            # print(f.shape)
+            f = f.set_index('combination')
+            feature_matrix = pd.concat([feature_matrix, f], axis=1)
+        i_anti+=1
+        feature_matrix = feature_matrix.drop(['initializer'], axis=1)# delete initializer column
+        feature_matrix = feature_matrix.fillna(0)
+
+        # feature_matrix = feature_matrix.T
+        # print(feature_matrix)
+        # feature_matrix.to_hdf(meta_temp+'_'+str(k)+'mer.h5', key='df', mode='w', complevel=9)  # overwriting.
+        print(feature_matrix)
+        exit()
 
 
 
-def extract_info(s,k,canonical,n_jobs):
+def extract_info(s,level,k,n_jobs):
     '''extract feature w.r.t. each species and drug
     k: k mer feature
     '''
 
-    # data = pd.read_csv('metadata/list_species_final_bq.txt', index_col=0,dtype={'genome_id': object}, sep="\t")
-    data = pd.read_csv('metadata/loose_Species_antibiotic_FineQuality.csv', index_col=0,
+    data = pd.read_csv('metadata/' + str(level) + '_Species_antibiotic_FineQuality.csv', index_col=0,
                        dtype={'genome_id': object}, sep="\t")
-    # data=data[data['number'] != 0]#drop the species with 0 in column 'number'.# quality controled data file Species_antibiotic_FineQuality.csv
-
-    #delete later:
-    #data=data.loc[['Escherichia coli'],:]
-    #data = data.loc[['Pseudomonas aeruginosa', 'Escherichia coli'], :]
-    #-------------
-    if s!=[]:
-        data = data.loc[s, :]
+    # data = pd.read_csv('metadata/Species_antibiotic_FineQuality.csv', index_col=0, dtype={'genome_id': object}, sep="\t")
+    data = data[data['number'] != 0]  # drop the species with 0 in column 'number'.
+    # for training model on part of the dataset:-------------
+    # data=data.loc[['Escherichia coli'],:]
+    data = data.loc[s, :]
+    # data.at['Mycobacterium tuberculosis', 'modelling antibiotics']=['capreomycin', 'ciprofloxacin']
+    # --------------------------------------------------------
     df_species = data.index.tolist()
+    antibiotics = data['modelling antibiotics'].tolist()
 
-    # antibiotics=data['modelling antibiotics'].tolist()
-    print(data)
-    print(k,canonical)
-
-    vocab=amr_utility.math_utility.vocab_build(canonical,k)# all the kmer combinations in list
-
-    pool = mp.Pool(processes=n_jobs)
-    pool.starmap(kmer, zip(df_species,repeat(k),repeat(canonical),repeat(vocab)))
-    #kmer('Pseudomonas aeruginosa',str(['ceftazidime', 'ciprofloxacin', 'meropenem']),k,canonical)
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    for species, antibiotics in zip(df_species, antibiotics):
+        log_addr=fileDir+'/log/temp/' + str(level) + '/' + str(species.replace(" ", "_"))
+        amr_utility.file_utility.make_dir(log_addr)
+        get_kmer(level,log_addr,k, species,antibiotics, n_jobs)
 
 
 if __name__== '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--k',default=13, type=int, required=True,
+    parser.add_argument('--k',default=13, type=int, required=False,
                         help='Kmer size')
     parser.add_argument('--n_jobs', default=1, type=int, help='Number of jobs to run in parallel.')
     # parser.add_argument('--a','--add', dest='odh',
     #                     help='Construct feature matrix with more samples, w.r.t. kmer features.',action='store_true',)#default:false
-    parser.add_argument('-c','--canonical', dest='canonical',action='store_true',
-                        help='Canonical kmer or not: True')
-    parser.add_argument('-n','--non_canonical',dest='canonical',action='store_false',
-                        help='Canonical kmer or not: False')
+    # parser.add_argument('-c','--canonical', dest='canonical',action='store_true',
+    #                     help='Canonical kmer or not: True')
+    # parser.add_argument('-n','--non_canonical',dest='canonical',action='store_false',
+    #                     help='Canonical kmer or not: False')
 
     parser.add_argument('-s', '--species', default=[], type=str, nargs='+', help='species to run: e.g.\'seudomonas aeruginosa\' \
                 \'Klebsiella pneumoniae\' \'Escherichia coli\' \'Staphylococcus aureus\' \'Mycobacterium tuberculosis\' \'Salmonella enterica\' \
                 \'Streptococcus pneumoniae\' \'Neisseria gonorrhoeae\'')
     parser.add_argument('-l', '--level', default='loose', type=str,
                         help='Quality control: strict or loose')
-    parser.add_argument('-cutoff', '--cutoff', default=1, type=int,
-                        help='specify frequency cut-off (default 1) for kmer counting. ')
+    # parser.add_argument('-cutoff', '--cutoff', default=1, type=int,
+    #                     help='specify frequency cut-off (default 1) for kmer counting. ')
 
     #parser.set_defaults(canonical=True)
     # parser.print_help()
     parsedArgs=parser.parse_args()
     print(parsedArgs)
-    extract_info(parsedArgs.species,parsedArgs.k,parsedArgs.canonical,parsedArgs.n_jobs)
+    extract_info(parsedArgs.species,parsedArgs.level,parsedArgs.k,parsedArgs.n_jobs)
 
 
