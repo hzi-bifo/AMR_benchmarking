fa82b96fa6ecf9ea50c63525f2afada151faa48c
augustkx <hukaixin1@outlook.com>
2023-10-18 13:10:34 +0200

    S2G2P

--- AMR_software/seq2geno/main_s2p.py
+++ AMR_software/seq2geno/main_s2p.py
@@ -93,7 +93,133 @@ def extract_info(path_sequence,temp_path,s,f_all,f_prepare_meta, f_phylotree, f_
             a_file.writelines(list_of_lines)
             a_file.close()
 
+    if f_ml:
 
+        for species, antibiotics in zip(df_species, antibiotics):
+            antibiotics, ID, Y = load_data.extract_info(species, False, level)
+            i_anti = 0
+            # antibiotics, ID, Y = antibiotics[9:11], ID[9:11], Y[9:11]
+            # antibiotics, ID, Y = antibiotics[11:], ID[11:], Y[11:]
+            for anti in antibiotics:
+
+                id_all = ID[i_anti]  # sample name list, e.g. [1352.10013,1352.10014,1354.10,1366.10]
+                y_all = Y[i_anti]
+                i_anti+=1
+                mer6_file ,_, _, _, _,log_feature,_ =  name_utility.GETname_S2Gfeature(species, temp_path,6)
+
+                s2g_file=log_feature+'results/RESULTS/bin_tables'
+                data_feature1 = pd.read_csv(s2g_file+'/gpa.mat_NONRDNT', index_col=0,sep="\t")
+                data_feature2 = pd.read_csv(s2g_file + '/indel.mat_NONRDNT',index_col=0,sep="\t")
+                data_feature3 = pd.read_hdf(mer6_file)
+                data_feature3 = data_feature3.T
+
+                init_feature = np.zeros((len(id_all), 1), dtype='uint16')
+
+
+                data_model_init = pd.DataFrame(init_feature, index=id_all, columns=['initializer'])
+                X_all = pd.concat([data_model_init, data_feature3.reindex(data_model_init.index)], axis=1)
+                #only scale the 6mer data
+                scaler = preprocessing.StandardScaler(with_mean=True, with_std=True).fit(X_all)
+                X_all = pd.DataFrame(data=scaler.transform(X_all),
+                               index=X_all.index,
+                               columns=X_all.columns)
+                X_all.index= 'iso_'+X_all.index
+                id_all=['iso_'+ s  for s in id_all]
+                X_all = pd.concat([X_all, data_feature1.reindex(X_all.index)], axis=1)
+                X_all = pd.concat([X_all, data_feature2.reindex(X_all.index)], axis=1)
+                X_all = X_all.drop(['initializer'], axis=1)
+
+                id_all = np.array(id_all)
+                y_all = np.array(y_all)
+
+
+                p_names = name_utility.GETname_meta(species,anti,level)
+                folds_txt=name_utility.GETname_folds(species,anti,level,f_kma,f_phylotree)
+                folders_sample = json.load(open(folds_txt, "rb"))
+                folders_index=name2index.Get_index(folders_sample,p_names) # CV folds
+                for chosen_cl in ['svm','lr', 'rf','lsvm']:
+                    _, _,save_name_score=name_utility.GETname_model('seq2geno', level,species, anti,chosen_cl,temp_path)
+                    file_utility.make_dir(os.path.dirname(save_name_score))
+
+                    hyper_space,cl=hyper_range.hyper_range(chosen_cl)
+                    # 1. by each classifier.2. by outer loop. 3. by inner loop. 4. by each hyper-para
+
+                    mcc_test = []  # MCC results for the test data
+                    f1_test = []
+                    score_report_test = []
+                    aucs_test = []
+                    hyperparameters_test = []
+                    score_InnerLoop=[]
+                    index_InnerLoop=[]
+                    cv_results_InnerLoop=[]
+                    predictY_test=[]
+                    true_Y=[]
+                    sampleNames_test=[]
+                    estimator_test=[]
+
+
+
+                    for out_cv in range(cv):
+                        print(species,anti,'. Starting outer: ', str(out_cv), '; chosen_cl: ', chosen_cl)
+
+
+                        test_samples_index = folders_index[out_cv]# a list of index
+                        id_test = id_all[test_samples_index]#sample name list
+                        y_test = y_all[test_samples_index]
+                        train_val_train_index =folders_index[:out_cv] +folders_index[out_cv + 1:]
+                        id_val_train = id_all[list(itertools.chain.from_iterable(train_val_train_index))]  # sample name list
+                        y_val_train = y_all[list(itertools.chain.from_iterable(train_val_train_index))]
+                        X_val_train=X_all.loc[id_val_train,:]
+                        X_test=X_all.loc[id_test,:]
+
+
+
+                        pipe = Pipeline(steps=[('cl', cl)])
+
+                        search = GridSearchCV(estimator=pipe, param_grid=hyper_space, n_jobs=n_jobs,
+                                                  scoring='f1_macro',
+                                                  cv=create_generator(train_val_train_index), refit=True)
+
+                        search.fit(X_all, y_all)
+                        hyperparameters_test_sub=search.best_estimator_
+                        scores_best=search.best_score_ #### July 2023. newly added. used to select the best classifiers for final outer loop testing.
+                        index_best=search.best_index_
+                        cv_results=search.cv_results_
+                        current_pipe=hyperparameters_test_sub
+                        # -------------------------------------------------------
+                        # retrain on train and val
+                        current_pipe.fit(X_val_train, y_val_train)
+                        y_test_pred = current_pipe.predict(X_test)
+                        # scores
+                        f1 = f1_score(y_test, y_test_pred, average='macro')
+                        report = classification_report(y_test, y_test_pred, labels=[0, 1], output_dict=True)
+                        mcc = matthews_corrcoef(y_test, y_test_pred)
+                        fpr, tpr, _ = roc_curve(y_test, y_test_pred, pos_label=1)
+                        roc_auc = auc(fpr, tpr)
+                        f1_test.append(f1)
+                        score_report_test.append(report)
+                        aucs_test.append(roc_auc)
+                        mcc_test.append(mcc)
+                        hyperparameters_test.append(hyperparameters_test_sub)
+                        score_InnerLoop.append(scores_best)
+                        index_InnerLoop.append(index_best)
+                        cv_results_InnerLoop.append(cv_results)
+                        predictY_test.append( y_test_pred.tolist())
+                        true_Y.append(y_test.tolist())
+                        sampleNames_test.append(folders_sample[out_cv])
+                        estimator_test.append(current_pipe)
+
+
+                    score ={'f1_test':f1_test,'score_report_test':score_report_test,'aucs_test':aucs_test,'mcc_test':mcc_test,
+                         'predictY_test':predictY_test,'ture_Y':true_Y,'samples':sampleNames_test}
+                    score2= {'hyperparameters_test':hyperparameters_test,'estimator_test':estimator_test,
+                             'score_InnerLoop':score_InnerLoop,'index_InnerLoop':index_InnerLoop,'cv_results_InnerLoop':cv_results_InnerLoop}
+                    with open(save_name_score + '_KMA_' + str(f_kma) + '_Tree_' + str(f_phylotree) + '.json',
+                              'w') as f:  # overwrite mode
+                        json.dump(score, f)
+                    with open(save_name_score + '_kma_' + str(f_kma) + '_tree_' + str(f_phylotree) + '_model.pickle',
+                              'wb') as f:  # overwrite mode
+                        pickle.dump(score2, f)
 
 
 
