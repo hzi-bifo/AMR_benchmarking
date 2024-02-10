1b5183ba008ae01c2533e5a44c6553854f66091f
augustkx <hukaixin1@outlook.com>
2024-02-10 01:36:50 +0100

    PATRIC TO NCBI mapping

--- src/data_preprocess/lib/ncbi_mapping.py
+++ src/data_preprocess/lib/ncbi_mapping.py
@@ -6,16 +6,13 @@ This script is to create the mapping file for PATRIC ID to NCBI ID. 9th Feb 2024
 
 
 data = pd.read_csv('./data/PATRIC/genome_metadata', dtype={'genome_id': object}, sep="\t")
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also#
-#     print(data.loc[data['genome_id'] == "1352.9931"])
-#     print("-------------------------------------------------")
-#     print(data.loc[data['genome_id'] == "1310800.122"])
 data = data.loc[:, ("genome_id", "biosample_accession","bioproject_accession","strain","assembly_accession","genbank_accessions","refseq_accessions","publication")]
 id_list=data["genome_id"].to_list()
 
+### genomes selected before QC (note: this list contains more genomes than our final datasets)
 data2=np.genfromtxt('./data/PATRIC/meta/genome_list', dtype="str")
 data=data.loc[data['genome_id'].isin(data2)]
-print(data)
+
 ### add SRA information
 ### run get_sra.sh
 data3 = pd.read_csv('./data/PATRIC/meta/sra.txt', dtype={' genome.genome_id': object}, sep="\t")
@@ -33,10 +30,9 @@ result.insert(3, 'sra_accession', second_column)
 result.to_csv('./data/PATRIC/meta/PATRICtoNCBI', sep="\t",  index = False)
 
 
-
+### check if all genome we selected are in the mapping file.
 # print(data2)
 # print(len(data))
-### check if all genome we selected are in the mapping file.
 ### 289012.3 is genome is gone from PATRIC, but we also did not include it in our datasets.
 # i=0
 # for each in data2:
@@ -47,9 +43,11 @@ result.to_csv('./data/PATRIC/meta/PATRICtoNCBI', sep="\t",  index = False)
 # # data = list(dict.fromkeys(data))
 # # print(len(data))
 
-## check if all genome we selected are equipped with at least NCBI and genbank
-
-
+## check if all genome we selected are equipped with at least NCBI and genbank. 327 such cases.
+result_check=result.loc[:, ("genome_id", "biosample_accession","bioproject_accession","sra_accession","genbank_accessions")]
+result_check.set_index('genome_id', inplace=True)
+selected_rows = result_check[result_check.isnull().all(axis=1)]
+print(selected_rows)
 
 
 
