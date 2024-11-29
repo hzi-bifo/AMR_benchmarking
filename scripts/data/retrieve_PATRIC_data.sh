2850078b912483b352f9a355a86a618c3553ca94
Kaixin <hukaixin1@outlook.com>
2024-11-29 14:00:56 +0100

    Update retrieve_PATRIC_data.sh

--- scripts/data/retrieve_PATRIC_data.sh
+++ scripts/data/retrieve_PATRIC_data.sh
@@ -4,7 +4,7 @@ data_dir="$1"
 
 
 #1. downloading PATRIC dataset
-for i in `cat ./doc/genome_list`;do
+for i in `cat ./data/PATRIC/meta/genome_list`;do
     if [ ! -f "$i/$i.fna" ]; then
 	 printf 'Downloading (%s)\n' "$i/$i.fna"
          wget -qN "ftp://ftp.patricbrc.org/genomes/$i/$i.fna" -P ${data_dir}
